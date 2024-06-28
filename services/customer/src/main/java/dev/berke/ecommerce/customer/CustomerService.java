1f012a7756fd7a3dbc6f65e1b44a917ec113ab67
Berke Sayın <sayinberke34@gmail.com>
2024-06-28 14:31:36 +0300

    Add createCustomer method

--- services/customer/src/main/java/dev/berke/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/dev/berke/ecommerce/customer/CustomerService.java
@@ -8,4 +8,10 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    public String createCustomer(CustomerRequest customerRequest) {
+        var customer = customerRepository.save(customerMapper.toCustomer(customerRequest));
+        return customer.getId();
+    }
 }
