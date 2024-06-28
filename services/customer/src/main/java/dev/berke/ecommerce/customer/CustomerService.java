fefb730735095c72365e15a2c277711b5db5d3ce
Berke Sayın <sayinberke34@gmail.com>
2024-06-28 14:50:27 +0300

    Implement updateCustomer method

--- services/customer/src/main/java/dev/berke/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/dev/berke/ecommerce/customer/CustomerService.java
@@ -1,7 +1,11 @@
 package dev.berke.ecommerce.customer;
 
+import dev.berke.ecommerce.exception.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,29 @@ public class CustomerService {
     public String createCustomer(CustomerRequest customerRequest) {
         var customer = customerRepository.save(customerMapper.toCustomer(customerRequest));
         return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest customerRequest) {
+        var customer = customerRepository.findById(customerRequest.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("Cannot update customer: No customer found with the provided ID: %s", customerRequest.id())
+                ));
+        mergeCustomer(customer, customerRequest);
+        customerRepository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest customerRequest) {
+        if (StringUtils.isNotBlank(customerRequest.firstname())) {
+            customer.setFirstname(customerRequest.firstname());
+        }
+        if (StringUtils.isNotBlank(customerRequest.lastname())) {
+            customer.setLastname(customerRequest.lastname());
+        }
+        if (StringUtils.isNotBlank(customerRequest.email())) {
+            customer.setEmail(customerRequest.email());
+        }
+        if (customerRequest.address() != null) {
+            customer.setAddress(customerRequest.address());
+        }
     }
 }
