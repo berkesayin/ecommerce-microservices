fe60ef7b5f53c1cf9c66709bae0e3d1840bdb479
Berke Sayın <sayinberke34@gmail.com>
2024-07-02 14:30:51 +0300

    Implement createProduct method

--- services/product/src/main/java/dev/berke/ecommerce/product/ProductController.java
+++ services/product/src/main/java/dev/berke/ecommerce/product/ProductController.java
@@ -1,6 +1,10 @@
 package dev.berke.ecommerce.product;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(@RequestBody @Valid ProductRequest productRequest) {
+        return ResponseEntity.ok(productService.createProduct(productRequest));
+    }
 }
