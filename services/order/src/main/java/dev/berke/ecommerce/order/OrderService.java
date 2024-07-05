471c885bc503c1c9b6ee2ea5751e111ee741e68c
Berke Sayın <sayinberke34@gmail.com>
2024-07-05 15:48:25 +0300

    Define the business logic steps for createOrder method

--- services/order/src/main/java/dev/berke/ecommerce/order/OrderService.java
+++ services/order/src/main/java/dev/berke/ecommerce/order/OrderService.java
@@ -9,4 +9,26 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+
+    public Integer createOrder(OrderRequest orderRequest) {
+        // check the customer (use OpenFeign)
+
+
+        // purchase the products (use product service, and RestTemplate)
+
+
+        // persist order (save order object)
+
+
+        // persist orderlines (save orderlines)
+
+
+        // payment process
+
+
+        // send the order confirmation (notification service, use kafka)
+
+
+        return null;
+    }
 }
