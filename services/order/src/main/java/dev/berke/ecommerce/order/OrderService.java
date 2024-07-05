76ab9bfcb5fa711f1f3ffe71aa4e3eb6c15fb70b
Berke Sayın <sayinberke34@gmail.com>
2024-07-05 16:18:39 +0300

    Fetch customer at order service to check if customer exists while creating order

--- services/order/src/main/java/dev/berke/ecommerce/order/OrderService.java
+++ services/order/src/main/java/dev/berke/ecommerce/order/OrderService.java
@@ -1,5 +1,7 @@
 package dev.berke.ecommerce.order;
 
+import dev.berke.ecommerce.customer.CustomerClient;
+import dev.berke.ecommerce.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,13 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final CustomerClient customerClient;
 
     public Integer createOrder(OrderRequest orderRequest) {
-        // check the customer (use OpenFeign)
 
+        // check the customer (use OpenFeign)
+        var customer = this.customerClient.findCustomerById(orderRequest.customerId())
+                .orElseThrow(() -> new BusinessException("Cannot create order:: No customer exists with the provided ID"));
 
         // purchase the products (use product service, and RestTemplate)
 
