ea62d00dd63ad72392499c335b5619a123f1edef
Berke Sayın <sayinberke34@gmail.com>
2024-07-16 15:37:36 +0300

    Start implementing createPayment method

--- services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentRequest.java
+++ services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentRequest.java
@@ -1,4 +1,13 @@
 package dev.berke.ecommerce.payment;
 
-public record PaymentRequest() {
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        Integer id,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        Integer orderId,
+        String orderReference,
+        Customer customer
+) {
 }
--- services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentService.java
@@ -9,4 +9,9 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+
+    public Integer createPayment(PaymentRequest paymentRequest) {
+        var payment = paymentRepository.save(paymentMapper.toPayment(paymentRequest));
+        return null;
+    }
 }
