07dbb96b59212303a0c5f23d95d3017f19401f52
Berke Sayın <sayinberke34@gmail.com>
2024-07-16 16:19:10 +0300

    Setup Kafka and sendNotification method

---
+++ services/payment/src/main/java/dev/berke/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package dev.berke.ecommerce.notification;
+
+import dev.berke.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
