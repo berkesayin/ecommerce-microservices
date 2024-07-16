a36c4372a2d5d69a88718486bd9ca87951bb4f4d
Berke Sayın <sayinberke34@gmail.com>
2024-07-16 16:29:24 +0300

    Complete implementation of createPayment method

--- services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/dev/berke/ecommerce/payment/PaymentService.java
@@ -1,5 +1,7 @@
 package dev.berke.ecommerce.payment;
 
+import dev.berke.ecommerce.notification.NotificationProducer;
+import dev.berke.ecommerce.notification.PaymentNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,21 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+    private final NotificationProducer notificationProducer;
 
     public Integer createPayment(PaymentRequest paymentRequest) {
         var payment = paymentRepository.save(paymentMapper.toPayment(paymentRequest));
-        return null;
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        paymentRequest.orderReference(),
+                        paymentRequest.amount(),
+                        paymentRequest.paymentMethod(),
+                        paymentRequest.customer().firstname(),
+                        paymentRequest.customer().lastname(),
+                        paymentRequest.customer().email()
+                )
+        );
+        return payment.getId();
     }
 }
