d9fb8c72bd168c6a40945497dc2b891629f50e8e
Berke Sayın <sayinberke34@gmail.com>
2024-07-17 15:48:07 +0300

    Setup EmailService

--- services/notification/src/main/java/dev/berke/ecommerce/NotificationApplication.java
+++ services/notification/src/main/java/dev/berke/ecommerce/NotificationApplication.java
@@ -2,8 +2,10 @@ package dev.berke.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NotificationApplication {
 
 	public static void main(String[] args) {
